d874ffc13d6bc260ed81b1797d513ee32924a19e
James Dawson <james@readsource.co.uk>
2013-10-09 22:49:12 +0000

    Simple build script to push resource providers to the local design-time 'PSProviders' folder and to a Pull Server.

---
+++ build.ps1
@@ -0,0 +1,14 @@
+﻿param
+(
+)
+
+$pullServerUnc = "C:\ProgramData\PSDSCPullServer\Modules"
+
+$resources = @(
+                "$pwd\AzurePublishingProfileResource",
+                "$pwd\AzureVmResource",
+                "$pwd\TimezoneResource"
+            )
+
+Write-Host "Publishing DSC Resource Providers"
+& "$pwd\..\publish.ps1" -resources $resources -pullServerUnc $pullServerUnc -updatePsProviders
