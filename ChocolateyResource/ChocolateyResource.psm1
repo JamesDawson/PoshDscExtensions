3b0dd8ee16f8e8d01a6b917fa0cad4b7dc0f7c5e
James Dawson <james@readsource.co.uk>
2013-10-10 20:41:58 +0100

    ChocolateyResource

---
+++ ChocolateyResource/ChocolateyResource.psm1
@@ -0,0 +1,112 @@
+﻿function Get-TargetResource
+{
+    param
+    (
+        [ValidateSet("present","absent")]
+        [string] $ensure,
+        [string] $package,
+        [string] $version
+    )
+
+    $res = @{}
+
+    # Is Chocolatey already installed?
+    if (Test-Path $chocolateyPath)
+    {
+        $res += @{ IsChocolateyInstalled = $true }
+
+        # Check if the package is already installed
+        $versionInfo = & $chocolateyPath Version $package -localOnly
+
+        if ($versionInfo.found -eq 'no version')
+        {
+            $res += @{ IsInstalled = $false }
+        }
+        else
+        {
+            $res += @{ IsInstalled = $true }
+            $res += @{ Version = $versionInfo.found }
+        }
+    }
+    else
+    {
+        $res += @{ IsChocolateyInstalled = $false }
+    }
+
+    return $res
+}
+
+function Test-TargetResource
+{
+    param
+    (
+        [ValidateSet("present","absent")]
+        [string] $ensure,
+        [string] $package,
+        [string] $version
+    )
+
+    $currentState = Get-TargetResource @PSBoundParameters
+
+    if ( !($currentState.IsChocolateyInstalled) )
+    {
+        Write-Verbose "Chocolatey is not installed"
+        return $false
+    }
+
+    if ($ensure -ieq "present" -and $currentState.IsInstalled)
+    {
+        Write-Verbose "Package is already installed"
+        return $true
+    }
+
+    if ( $ensure -ieq "absent" -and !($currentState.IsInstalled) )
+    {
+        Write-Verbose "Package is already not installed"
+        return $true
+    }
+
+    # If we get this far we can assume that we're out of policy
+    return $false
+}
+
+function Set-TargetResource
+{
+    param
+    (
+        [ValidateSet("present","absent")]
+        [string] $ensure,
+        [string] $package,
+        [string] $version
+    )
+
+    $currentState = Get-TargetResource @PSBoundParameters
+
+    if ( !($currentState.IsChocolateyInstalled) )
+    {
+        _installChocolatey
+    }
+
+    if ($ensure -ieq "present")
+    {
+        & $chocolateyPath install $package
+    }
+    else
+    {
+        & $chocolateyPath uninstall $package
+    }
+}
+
+function _installChocolatey
+{
+    Write-Verbose "Installing Chocolatey"
+    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+# default chocolatey install path
+$chocolateyPath = "C:\Chocolatey\chocolateyinstall\chocolatey.ps1"
+# attempt to find an existing install path
+if ($env:ChocolateyInstall)
+{
+    $chocolateyPath = "$env:ChocolateyInstall\chocolateyinstall\chocolatey.ps1"
+}
